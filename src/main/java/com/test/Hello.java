60730dd21bb7a76645a3f08c55f10934fc128380
Farouk Talha <farouk.talha@gmail.com>
2025-12-01 17:14:51 +0100

    Webhook test

--- src/main/java/com/test/Hello.java
+++ src/main/java/com/test/Hello.java
@@ -2,6 +2,6 @@ package com.test;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World from Maven!");
+        System.out.println("Hello from Jenkins automation!!");
     }
 }
